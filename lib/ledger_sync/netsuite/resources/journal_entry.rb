0a227d00ab905b477be1fa67f35cf67250c32eff
Ryan Jackson <ryanwjackson@gmail.com>
2021-05-28 18:26:55 +0200

    Add class to journal entry

--- lib/ledger_sync/netsuite/resources/journal_entry.rb
+++ lib/ledger_sync/netsuite/resources/journal_entry.rb
@@ -13,6 +13,7 @@ module LedgerSync
 
       references_one :currency, to: Currency
       references_one :subsidiary, to: Subsidiary
+      references_one :ledger_class, to: LedgerClass
 
       references_many :line_items, to: JournalEntryLineItem
 

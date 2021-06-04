85863363b6d423c0cff4011792685c9a23d026bf
Jason Y. Jong <joulesqrd@gmail.com>
2021-06-04 12:04:08 -0500

    Support JournalEntryLineItem class + entity (#10)
    Co-authored-by: Jason Jong <jason@Jasons-MacBook-Pro.local>

--- lib/ledger_sync/netsuite/journal_entry_line_item/serializer.rb
+++ lib/ledger_sync/netsuite/journal_entry_line_item/serializer.rb
@@ -10,6 +10,11 @@ module LedgerSync
         attribute :credit
         attribute :debit
         attribute :memo
+        references_one :entity,
+                       serializer: Reference::Serializer
+        references_one :class,
+                       resource_attribute: :ledger_class,
+                       serializer: Reference::Serializer
         references_one :account,
                        serializer: Reference::Serializer
       end
